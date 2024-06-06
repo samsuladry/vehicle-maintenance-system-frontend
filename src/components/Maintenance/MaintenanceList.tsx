381b1bc3b5ba68a294b91b59f6261149e8f5d1dd
samsuladry <samsuladry@gmail.com>
2024-06-07 01:32:11 +0800

    update sorting

--- src/components/Maintenance/MaintenanceList.tsx
+++ src/components/Maintenance/MaintenanceList.tsx
@@ -1,28 +1,70 @@
 import React, { useEffect, useState } from 'react';
 import { useMaintenanceStore } from '../../store/maintenanceStore';
 import { useVehicleStore } from '../../store/vehicleStore';
-import { PencilSquareIcon, TrashIcon, NoSymbolIcon } from "@heroicons/react/24/solid";
+import { PencilSquareIcon, TrashIcon, LockClosedIcon, ChevronDownIcon, ChevronUpIcon } from "@heroicons/react/24/solid";
 import DeleteConfirmationModal from '../Modal/DeleteConfirmation';
 import EditMaintenanceModal from '../Modal/EditMaintenanceModal';
+
+type Vehicle = {
+  id: number;
+  name: string;
+  type: string;
+  colour: string;
+  createdAt: string;
+  updatedAt: string;
+};
+
+type Maintenance = {
+  id: number;
+  vehicleId: number;
+  status: string;
+  createdAt: string;
+  updatedAt: string;
+};
+
+type VehicleColumns = 'name' | 'type' | 'colour' | 'createdAt' | 'updatedAt';
 
 const MaintenanceList = () => {
   const { maintenanceList, fetchMaintenance, deleteMaintenance } = useMaintenanceStore();
   const { vehicleList, fetchVehicles } = useVehicleStore();
   const [showDeleteModal, setShowDeleteModal] = useState(false);
   const [maintenanceToDelete, setMaintenanceToDelete] = useState<number | null>(null);
-  
   const [showEditModal, setShowEditModal] = useState(false);
   const [maintenanceToEdit, setMaintenanceToEdit] = useState<number | null>(null);
+  const [order, setOrder] = useState(true); // true for asc
+  const [sortedList, setSortedList] = useState<Maintenance[]>([]);
 
   useEffect(() => {
     fetchVehicles();
     fetchMaintenance();
   }, [fetchVehicles, fetchMaintenance]);
 
+  useEffect(() => {
+    setSortedList(maintenanceList);
+  }, [maintenanceList]);
+
+  const sorting = (col: VehicleColumns) => {
+    const sorted = [...sortedList].sort((a, b) => {
+      const vehicleThatMatchMaintenanceA = vehicleList.find((v) => v.id === a.vehicleId);
+      const vehicleThatMatchMaintenanceB = vehicleList.find((v) => v.id === b.vehicleId);
+
+      if (vehicleThatMatchMaintenanceA && vehicleThatMatchMaintenanceB) {
+        if (order) {
+          return vehicleThatMatchMaintenanceA[col].toString().localeCompare(vehicleThatMatchMaintenanceB[col].toString(), undefined, { sensitivity: 'base' });
+        } else {
+          return vehicleThatMatchMaintenanceB[col].toString().localeCompare(vehicleThatMatchMaintenanceA[col].toString(), undefined, { sensitivity: 'base' });
+        }
+      }
+      return 0;
+    });
+    setOrder(!order);
+    setSortedList(sorted);
+  };
+
   const handleEdit = async (id: number) => {
-    setMaintenanceToEdit(id)
-    setShowEditModal(true)
-  }
+    setMaintenanceToEdit(id);
+    setShowEditModal(true);
+  };
 
   const confirmDelete = (id: number) => {
     setMaintenanceToDelete(id);
@@ -33,14 +75,14 @@ const MaintenanceList = () => {
     if (maintenanceToDelete !== null) {
       try {
         await deleteMaintenance(maintenanceToDelete);
-        await fetchVehicles();
+        await fetchMaintenance();
         setShowDeleteModal(false);
         setMaintenanceToDelete(null);
       } catch (error) {
-        console.error('Failed to delete vehicle:', error);
+        console.error('Failed to delete maintenance:', error);
       }
     }
-  }
+  };
 
   return (
     <div className="container mx-auto">
@@ -64,7 +106,12 @@ const MaintenanceList = () => {
             <table className="table-auto w-full border-collapse">
               <thead>
                 <tr>
-                  <th className="px-10 py-5 border">Vehicle Name</th>
+                  <th className="px-10 py-5 border cursor-pointer" onClick={() => sorting('name')}>
+                    <div className='flex items-center justify-center'>
+                      <span className='pr-2'>Vehicle Name</span>
+                      { order ? <ChevronDownIcon className="h-6 w-6"/> : <ChevronUpIcon className="h-6 w-6"/>}
+                    </div>
+                  </th>
                   <th className="px-10 py-5 border">Status</th>
                   <th className="px-10 py-5 border">Created At</th>
                   <th className="px-10 py-5 border">Updated At</th>
@@ -72,7 +119,7 @@ const MaintenanceList = () => {
                 </tr>
               </thead>
               <tbody>
-                {maintenanceList.map((maint) => {
+                {sortedList.map((maint) => {
                   const vehicle = vehicleList.find((v => v.id === maint.vehicleId));
                   return (
                     <tr key={maint.id}>
@@ -82,8 +129,8 @@ const MaintenanceList = () => {
                       <td className="px-10 py-5 border text-lg font-semibold">{new Date(maint.updatedAt).toLocaleString()}</td>
 
                       <td className="px-10 py-5 border text-lg font-semibold">
-                        {maint && maint.status && maint.status.toLocaleLowerCase() !== 'done' ? (
-                          <div className='flex items-center space-x-4'>
+                        {maint && maint.status && maint.status.toLowerCase() !== 'done' ? (
+                          <div className='flex justify-center space-x-4'>
                             <button onClick={() => handleEdit(maint.id)}>
                               <PencilSquareIcon className="h-6 w-6"/>
                             </button>
@@ -91,13 +138,14 @@ const MaintenanceList = () => {
                               <TrashIcon className="h-6 w-6"/>
                             </button>
                           </div>
-                        ) : <NoSymbolIcon className="h-6 w-6"/>}
+                        ) : (
+                        <div className='flex justify-center'>
+                          <LockClosedIcon className="h-6 w-6"/>
+                        </div>
+                        )}
                       </td>
-
-
                     </tr>
-                  )
-                  
+                  );
                 })}
               </tbody>
             </table>
--- src/components/Vehicle/VehicleList.tsx
+++ src/components/Vehicle/VehicleList.tsx
@@ -1,8 +1,19 @@
 import { useEffect, useState } from 'react';
 import { useVehicleStore } from '../../store/vehicleStore';
-import { PencilSquareIcon, TrashIcon } from "@heroicons/react/24/solid";
+import { PencilSquareIcon, TrashIcon, ChevronDownIcon, ChevronUpIcon } from "@heroicons/react/24/solid";
 import DeleteConfirmationModal from '../Modal/DeleteConfirmation';
 import EditModal from '../Modal/EditVehicleModal';
+
+type Vehicle = {
+  id: number;
+  name: string;
+  type: string;
+  colour: string;
+  createdAt: string;
+  updatedAt: string;
+};
+
+type VehicleColumns = 'name' | 'type' | 'colour' | 'createdAt' | 'updatedAt';
 
 const VehicleList = () => {
   const { vehicleList, fetchVehicles, deleteVehicle } = useVehicleStore();
@@ -10,15 +21,33 @@ const VehicleList = () => {
   const [vehicleToDelete, setVehicleToDelete] = useState<number | null>(null);
   const [showEditModal, setShowEditModal] = useState(false);
   const [vehicleToEdit, setVehicleToEdit] = useState<number | null>(null);
+  const [order, setOrder] = useState(true); // true for asc
+  const [sortedList, setSortedList] = useState<Vehicle[]>(vehicleList);
 
   useEffect(() => {
     fetchVehicles();
   }, [fetchVehicles]);
 
+  useEffect(() => {
+    setSortedList(vehicleList);
+  }, [vehicleList]);
+
+  const sorting = (col: VehicleColumns) => {
+    const sorted = [...sortedList].sort((a, b) => {
+      if (order) {
+        return a[col].toString().toLowerCase() > b[col].toString().toLowerCase() ? 1 : -1;
+      } else {
+        return a[col].toString().toLowerCase() < b[col].toString().toLowerCase() ? 1 : -1;
+      }
+    });
+    setOrder(!order);
+    setSortedList(sorted);
+  };
+
   const handleEdit = async (id: number) => {
     setVehicleToEdit(id); 
     setShowEditModal(true); 
-  }
+  };
 
   const confirmDelete = (id: number) => {
     setVehicleToDelete(id);
@@ -36,8 +65,7 @@ const VehicleList = () => {
         console.error('Failed to delete vehicle:', error);
       }
     }
-  }
-  
+  };
 
   return (
     <div className='container mx-auto'>
@@ -56,12 +84,21 @@ const VehicleList = () => {
 
       <h1 className='text-4xl font-bold text-center my-4'>Vehicle List</h1>
       <div className='flex flex-wrap justify-center'>
-        {vehicleList.length > 0 ? (
+        {sortedList.length > 0 ? (
           <div className='overflow-x-auto'>
             <table className='table-auto w-full border-collapse'>
               <thead>
                 <tr>
-                  <th className='px-10 py-5 border'>Name</th>
+                  <th className='px-10 py-5 border cursor-pointer' onClick={() => sorting('name')}>
+                    <div className='flex flex-wrap items-center justify-center'>
+                      <div className='pr-2'>
+                        Name 
+                      </div>
+                      <div>
+                        { order ? <ChevronUpIcon className="h-6 w-6"/> : <ChevronDownIcon className="h-6 w-6"/>}
+                      </div>
+                    </div>
+                  </th>
                   <th className='px-10 py-5 border'>Type</th>
                   <th className='px-10 py-5 border'>Colour</th>
                   <th className='px-10 py-5 border'>CreatedAt</th>
@@ -70,16 +107,14 @@ const VehicleList = () => {
                 </tr>
               </thead>
               <tbody>
-                {vehicleList.map((vehicle) => (
+                {sortedList.map((vehicle) => (
                   <tr key={vehicle.id}>
-                    {}
                     <td className='border px-10 py-5 text-lg font-semibold'>{vehicle.name}</td>
                     <td className='border px-10 py-5 text-lg font-semibold'>{vehicle.type}</td>
                     <td className='border px-10 py-5 text-lg font-semibold'>{vehicle.colour}</td>
                     <td className='border px-10 py-5 text-lg font-semibold'>{new Date(vehicle.createdAt).toLocaleString()}</td>
                     <td className='border px-10 py-5 text-lg font-semibold'>{new Date(vehicle.updatedAt).toLocaleString()}</td>
-                    
-                    <td className='border px-10 py-5 text-lg font-semibold'>{
+                    <td className='border px-10 py-5 text-lg font-semibold'>
                       <div className='flex items-center space-x-4'>
                         <button onClick={() => handleEdit(vehicle.id)}>
                           <PencilSquareIcon className="h-6 w-6"/>
@@ -88,9 +123,7 @@ const VehicleList = () => {
                           <TrashIcon className="h-6 w-6"/>
                         </button>
                       </div>
-
-                      } </td>
-                    
+                    </td>
                   </tr>
                 ))}
               </tbody>
